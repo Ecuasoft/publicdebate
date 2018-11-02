09aff8a23917445e6e418a242a51b90d015698b1
Andrés Villenas <andresvillenas@hotmail.com>
2018-11-02 10:11:38 -0500

    Initial components of angular

---
+++ PublicDebate.Web/ClientApp/src/app/modules/account/account.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SignUpComponent]
+})
+export class AccountModule { }
